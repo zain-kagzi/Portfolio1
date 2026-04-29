f245c09f9c5b203eeb9b7e488f39713f2c78940e
Zain_Kagzi <113235988+zain-kagzi@users.noreply.github.com>
2026-04-29 18:55:37 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -37,7 +37,7 @@ const About = () => {
             mb-2!
             font-display
           ">
-            BEST FULL STACK
+            FULL STACK
             <br /> DEVELOPER
           </h2>
 
